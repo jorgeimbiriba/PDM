1b5f5994fd57b1d432e8da4f23c27cfa0a4e9deb
Jorge Henrique <jorge.henrique7171@gmail.com>
2025-08-12 21:26:31 -0400

    Finalizado até a questão 3

---
+++ Primeira Lógica/Lista01/src/Questao3.kt
@@ -0,0 +1,19 @@
+fun comprimentoOuZero(palavra: String?): Int = palavra?.length ?: 0
+
+fun descreveEndereco(rua: String?, numero: Int, cidade:String?): String{
+    val ruaDescricao = rua ?: "Não informado"
+    val numeroDescricao = numero?.toString() ?: "Não informado"
+    val cidadeDescricao = cidade ?: "Não informado"
+
+    return "Rua $ruaDescricao, Nº $numeroDescricao, $cidadeDescricao"
+}
+
+fun main(){
+    val palavra = "Paralelepípedo"
+    val resultado = comprimentoOuZero(palavra)
+    println("$palavra")
+    println("Nº de caracteres: $resultado")
+
+    val endereco1 = descreveEndereco("Barão do Rio Preto", 123, "Manaus")
+    println(endereco1)
+}
