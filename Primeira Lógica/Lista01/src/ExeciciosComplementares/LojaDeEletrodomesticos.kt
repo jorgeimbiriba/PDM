96204d5eb3c455ec6d525e3296ad85883dbe2ca1
Jorge Henrique <jorge.henrique7171@gmail.com>
2025-08-13 10:51:52 -0400

    Começo das primeiras classes para as atvs complementares

---
+++ Primeira Lógica/Lista01/src/ExeciciosComplementares/LojaDeEletrodomesticos.kt
@@ -0,0 +1,5 @@
+package ExeciciosComplementares
+
+class Televisores(val marca: String, val modelo: String,val tipo:String, val preco:Double)
+
+class empregado(val nome: String, val quantidadeDeVendas: Int, val salario: Double)
