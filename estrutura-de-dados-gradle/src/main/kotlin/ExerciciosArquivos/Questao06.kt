034e6b0f7f9be620853b876654cfa1400d81426a
Jorge Henrique <jorge.henrique7171@gmail.com>
2025-08-30 19:01:09 -0400

    Questão 5 finalizada

---
+++ estrutura-de-dados-gradle/src/main/kotlin/ExerciciosArquivos/Questao06.kt
@@ -0,0 +1,40 @@
+package ExerciciosArquivos
+
+import java.io.File
+import java.io.IOException
+
+data class Pessoa (val nome: String, val email: String, val idade: Int )
+
+fun criarArquivoCSV(nomeArquivo: String, pessoa: Pessoa){
+    val arquivo = File(nomeArquivo)
+
+    try {
+        if (!arquivo.exists()){
+            arquivo.writeText("nome,email,idade\n")
+        }
+
+        val linhaCSV = "${pessoa.nome}, ${pessoa.email}, ${pessoa.idade}\n"
+
+        arquivo.appendText(linhaCSV)
+
+        println("'${pessoa.nome}' adicionado com sucesso ao arquivo '$nomeArquivo'.")
+    }catch (e: IOException){
+        println("Erro ao adicionar a pessoa: ${e.message}")
+    }
+}
+
+fun main(){
+    val arquivoNome = "dados_pessoais.csv"
+
+
+    val pessoa1 = Pessoa("Ana", "ana@email.com", 25)
+    criarArquivoCSV(arquivoNome, pessoa1)
+
+
+    val pessoa2 = Pessoa("Carlos", "carlos@email.com", 30)
+    criarArquivoCSV(arquivoNome, pessoa2)
+
+    
+    println("\nConteúdo do arquivo '$arquivoNome':")
+    File(arquivoNome).readLines().forEach { println(it) }
+}
